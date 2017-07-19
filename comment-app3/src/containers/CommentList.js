bf75d71d420431f4eb5c96fe515de8477f53e09b
zhuqianye <q835041058@gmail.com>
2017-07-19 14:01:35 +0800

    I have know about the smart component whick name CommentList

--- comment-app3/src/containers/CommentList.js
+++ comment-app3/src/containers/CommentList.js
@@ -8,8 +8,12 @@ import PropTpyes from 'prop-types';
 import CommentList from '../components/CommentList';
 import {
 	initComments,
-	deleteComment
+	deleteComments
 } from '../reducers/comment.js';
+//	我终于理解了。是这样的.
+// 1.在WillMount里面我们从localStorage获取原来的comments，通过JSON.parse将字符串解析为JSON格式
+// 2.其后立马通过this.initComments()这个函数将其与state绑定
+// 3.之后在render()中组件便可以获取到props中的comments了
 
 // CommentListContainer
 // 一个Smart组件，负责评论列表加载，初始化和删除
@@ -18,17 +22,21 @@ class CommentListContainer extends Component {
 		comments: PropTpyes.array,
 		initComments: PropTpyes.func,
 		onDeleteComment: PropTpyes.func
+
+	}
+	componentWillMount() {
+		this._loadComments();
+	}
+	_loadComments() {
+		let comments = localStorage.getItem('comments');
+		comments = comments ? JSON.parse() : [];
+		this.initComments(comments);
 	}
 
 	handleDeleteComment(index) {
 		if (!!this.props.onDeleteComment) {
 			this.props.onDeleteComment(index);
 		}
-	}
-
-	_loadComments() {
-		let comments = localStorage.getItem('comments');
-		comments = comments ? JSON.parse() : [];
 	}
 	render() {
 		return (<CommentList comments={this.props.comments} 
