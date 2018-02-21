578d6c76e6ac3ef7daf5a56a247ca93615da30d7
zhuqianye <q835041058@gmail.com>
2018-02-21 21:43:22 +0800

    learn

--- 2_react_book/src/index.js
+++ 2_react_book/src/index.js
@@ -1,8 +1,25 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
+// import App from './App';
 import registerServiceWorker from './registerServiceWorker';
+class Title extends React.Component {
+  handleClick(e) {
+    console.log(this);
+    console.log(e.target.innerHTML);
+    console.log('click Title');
+  }
+  render() {
+    return <h1 onClick={this.handleClick.bind(this)}>React 小书</h1>
+  }
+}
+class Header extends React.Component {
+  render() {
+    return <div>
+      <span>1.</span>
+      <Title />
+    </div>
+  }
+}
 
-ReactDOM.render(<App />, document.getElementById('root'));
+ReactDOM.render(<Header />, document.getElementById('root'));
 registerServiceWorker();
