adf1115371af4d7547902b8b717da5f1939ed4e6
zhuqianye <q835041058@gmail.com>
2018-05-07 00:10:26 +0800

    editor demo1-1

--- markdown-editor/src/App.js
+++ markdown-editor/src/App.js
@@ -6,14 +6,23 @@ class App extends Component {
   constructor(props) {
     super(props);
     this.state = {
-      previewContent: ''
+      previewContent: '',
+      aceBoxH: null
     }
-  } 
+    this.cacheValue();
+    this.wrapScroll = this.wrapScroll.bind(this);
+    this.onContentChange = this.onContentChange.bind(this);
+  }
+
+  cacheValue() {
+    this.currentTabIndex = 1;
+  }
+
   render() {
     let state = this.state;
 
     return (<div id="app">
-      <header key='header'>
+      <header key='header' className="edit-header">
         <input type="text" placeholder="输入文章标题..." spellCheck="false" />
       </header>
       <div key='main'
@@ -21,15 +30,21 @@ class App extends Component {
         ref={node => this.aceBox = node}
         style={{height:state.aceBoxH}}>
 
-        <div className="edit-wrap" ref={node => this.editWrap = node}>
+        <div className="edit-wrap"
+             onMouseOver={this.setCurrentIndex.bind(this, 1)}
+             onScroll={this.wrapScroll}
+             ref={node => this.editWrap = node}>
           <div className="edit-box"
                contentEditable="plaintext-only"
-               onInput={this.onContentChange.bind(this)}
+               onInput={this.onContentChange}
                ref={node => this.editBox = node}>
           </div>
         </div>
 
-        <div className="mark-wrap" ref={node => this.markWrap = node}>
+        <div className="mark-wrap"
+             onMouseOver={this.setCurrentIndex.bind(this, 2)}
+             onScroll={this.wrapScroll}
+             ref={node => this.markWrap = node}>
           <div className="mark-box"
                ref={node => this.markBox = node}
                dangerouslySetInnerHTML={{__html: state.previewContent}}>
@@ -39,10 +54,26 @@ class App extends Component {
     </div>)
   }
 
+  componentDidMount() {
+    let clientHeight = document.documentElement.clientHeight,
+        offsetHeight = document.querySelector('.edit-box').offsetHeight;
+    this.setState({
+      aceBoxH: clientHeight - offsetHeight + "px"
+    });
+  }
+
   onContentChange(e) {
     this.setState({
       previewContent: marked(e.target.innerText)
     });
+  }
+
+  setCurrentIndex(index) {
+    this.currentTabIndex = index;
+  }
+
+  wrapScroll() {
+    console.log('e');
   }
 }
 
